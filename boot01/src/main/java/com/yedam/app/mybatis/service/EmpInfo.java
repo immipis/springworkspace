d9b97c04553080c4289f6c6b1c8c154c060822f5
immipis <132124793+immipis@users.noreply.github.com>
2025-02-14 17:23:18 +0900

    123
    123

--- boot01/src/main/java/com/yedam/app/mybatis/service/EmpInfo.java
+++ boot01/src/main/java/com/yedam/app/mybatis/service/EmpInfo.java
@@ -15,4 +15,5 @@ public class EmpInfo {
 	private String lastName;
 	private Date hireDate;
 	private double salary;
+	private int jobId;
 }
--- boot01/src/test/java/com/yedam/app/Boot01ApplicationTests.java
+++ boot01/src/test/java/com/yedam/app/Boot01ApplicationTests.java
@@ -3,6 +3,9 @@ package com.yedam.app;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -40,15 +43,53 @@ class Boot01ApplicationTests {
 //		// => 두개가 같으면 테스트 성공, 다르면 실패
 //	}
 	
-	@Test
-	void insertTest() {
+//	@Test
+	void insertTest() throws ParseException{
 		EmpVO empVO = new EmpVO();
-		empVO.setLastName("KANG");
-		empVO.setEmail("kang@daum.net");
+		empVO.setLastName("jo");
+		empVO.setEmail("jo1231221@daum.net");
 		empVO.setJobId("SA_REP");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+		Date date = sdf.parse("240501");
+		
+		empVO.setHireDate(date);
 		
 		int result = empMapper.insertEmpInfo(empVO);
 		
+		assertEquals(1, result);
+	}
+	
+//	@Test
+//	void upDateTest() {
+//		EmpVO empVO = new EmpVO();
+//		empVO.setEmployeeId(20);
+//		
+//		EmpVO findVO = empMapper.selectEmpInfo(empVO);
+//		
+//		findVO.setLastName("asd");
+//		findVO.setJobId(null);
+//		
+//		int result = empMapper.updateEmpInfo(findVO);
+//		
+//		System.out.println(result);
+//		assertEquals(1, result);
+//	}
+//	@Test
+//    void delTest() {
+//		int result = empMapper.deleteEmpInfo(235);
+//		assertEquals(1, result);
+//	}
+	
+	@Test
+	void selectKeyTest() {
+		EmpVO empVO = new EmpVO();
+		empVO.setLastName("jo");
+		empVO.setEmail("jo99991@daum.net");
+		empVO.setJobId("SA_REP");
+		// employeeId = null
+		int result = empMapper.insertEmpInfo(empVO);
+		System.out.println("===="+empVO.getEmployeeId());
 		assertEquals(1, result);
 	}
 }
